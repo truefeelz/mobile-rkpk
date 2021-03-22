6ea1ffdbdbb01fe6ad42563866b50a1e147ba081
Чеканов Кирилл Александрович <Chekanov.K.Al@sberbank.ru>
2021-03-22 13:51:41 +0500

    Добавит metricItem

--- src/pages/KpiPage/KpiPage.tsx
+++ src/pages/KpiPage/KpiPage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import classes from './KpiPage.module.css';
+import {MetricItem} from "./components/metricItem";
+import {metrics} from "./metrics";
 
 export const KpiPage: React.FC = () => {
     return (
         <div className={classes.page}>
-          KpiPage
+            {metrics.map(({ title, subtitle, imagePath, metric, delta}) => (
+                <MetricItem
+                    title={title}
+                    subtitle={subtitle}
+                    imagePath={imagePath}
+                    metric={metric}
+                    delta={delta}
+                />
+            ))}
         </div>
     );
 };
---
+++ src/pages/KpiPage/components/metricItem/MetricItem.tsx
@@ -0,0 +1,37 @@
+import React, {ReactNode} from "react";
+import classes from "./MetricItem.module.css";
+
+interface IProps {
+    title: string;
+    subtitle: string;
+    imagePath: string | null;
+    metric: number | null;
+    delta: number | null;
+}
+
+export const MetricItem: React.FC<IProps> = (props) => {
+    const { title, subtitle, imagePath, metric, delta} = props;
+
+    const deltaSign = delta ? ((delta < 0) ? '-' : (delta > 0) ? '+' : '') : '--';
+    const deltaClasses = delta ? ((delta < 0) ? classes.deltaRed : classes.deltaGreen) : classes.deltaWhite;
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.titleContainer}>
+                <div className={classes.title}>{title}</div>
+                <div className={classes.subtitle}>{subtitle}</div>
+            </div>
+            <div className={classes.infoContainer}>
+                <div className={classes.graphContainer}>
+                    {imagePath && <img src={imagePath} className={classes.graph} />}
+                </div>
+                <div className={classes.metricsContainer}>
+                    <div className={classes.metric}>{metric ?? '-'}</div>
+                    <div className={deltaClasses}>
+                        {deltaSign}{delta}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
