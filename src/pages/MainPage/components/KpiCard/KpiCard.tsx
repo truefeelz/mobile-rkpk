dec3a0345fe21ff7212fe3cc0ad5ab9aef7fd3e0
Чеканов Кирилл Александрович <Chekanov.K.Al@sberbank.ru>
2021-03-22 17:55:36 +0500

    Добавит Card

--- src/pages/MainPage/components/KpiCard/KpiCard.tsx
+++ src/pages/MainPage/components/KpiCard/KpiCard.tsx
@@ -2,36 +2,39 @@ import React from 'react';
 import classes from './KpiCard.module.css';
 import { MetricItem } from "./components/metricItem";
 import { metrics } from "./metrics";
+import {Card} from "../../../components";
 
 export const KpiCard: React.FC = () => {
     return (
-        <div className={classes.page}>
-          <div className={classes.pageHeader}>
-              <div className={classes.container}>
-                  <span className={classes.title}>
-                      КПЭ
-                  </span>
-                  <span className={classes.subTitle}>
-                      1 – 5 Февраля
-                  </span>
+        <Card title="КПЭ">
+            <div className={classes.page}>
+              <div className={classes.pageHeader}>
+                  <div className={classes.container}>
+                      <span className={classes.title}>
+                          КПЭ
+                      </span>
+                      <span className={classes.subTitle}>
+                          1 – 5 Февраля
+                      </span>
+                  </div>
               </div>
-          </div>
-          <div className={classes.pageContent}>
-              <div className={classes.container}>
-                  {metrics.map(({ title, subtitle, imagePath, metric, delta}) => (
-                      <MetricItem
-                          title={title}
-                          subtitle={subtitle}
-                          imagePath={imagePath}
-                          metric={metric}
-                          delta={delta}
-                      />
-                  ))}
+              <div className={classes.pageContent}>
+                  <div className={classes.container}>
+                      {metrics.map(({ title, subtitle, imagePath, metric, delta}) => (
+                          <MetricItem
+                              title={title}
+                              subtitle={subtitle}
+                              imagePath={imagePath}
+                              metric={metric}
+                              delta={delta}
+                          />
+                      ))}
+                  </div>
               </div>
-          </div>
-          <div className={classes.pageFooter}>
+              <div className={classes.pageFooter}>
 
-          </div>
-        </div>
+              </div>
+            </div>
+        </Card>
     );
 };
